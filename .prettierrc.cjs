07afa9c5ab06f10f8c5fa3cfa478e5e24fdd4c63
Bahram <bahramsa70@gmail.com>
2022-09-11 09:15:44 +0430

    Completing eslint, prettier, irbnb

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
